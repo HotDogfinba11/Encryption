a858bcb3687502ed8f09a81dfdd9b286940d5527
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2018-11-21 22:07:09 +0000

    Add files via upload

--- Decrypt & Encrypt/main.cpp
+++ Decrypt & Encrypt/main.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main()
 {
    int i, x;
-   char str[100];
+   char str[1000];
 
-   cout << "Please enter a string without spaces:\n";
-   cin >> str;
+
+
+   cout << "Please enter a string:\n";
+   cin.getline (str,1000); //included to stop the program from crashing if there was a space input
+
 
    cout << "\nPlease choose following options:\n";
    cout << "1 = Encrypt the string.\n";
@@ -19,7 +22,7 @@ int main()
    {
       //Encrypt string case
       case 1:
-         for(i = 0; (i < 100 && str[i] != '\0'); i++)
+         for(i = 0; (str[i] != '\0'); i++)
             str[i] = str[i] + 3; //the key for encryption is 3 that is added to ASCII value
 
          cout << "\nEncrypted string: " << str << endl;
@@ -27,14 +30,18 @@ int main()
 
       //Encrypt string case
       case 2:
-         for(i = 0; (i < 100 && str[i] != '\0'); i++)
+         for(i = 0; (str[i] != '\0'); i++)
          str[i] = str[i] - 3; //the key for encryption is 3 that is subtracted to ASCII value
 
       cout << "\nDecrypted string: " << str << endl;
       break;
 
       default:
-         cout << "\nInvalid Input !!!\n";
+         cout << "\nInvalid Input\n";
+
    }
+   std::cin.clear();
+   std::cin.sync();
+   std::cin.get();
    return 0;
 }
